eddea118fa7886546cad580bed27c34ea5a1222e
pkrucz00 <pawel.kruczkiewicz@gmail.com>
2023-03-05 10:07:26 +0100

    Throughtput computation

--- mpi/thrtp.c
+++ mpi/thrtp.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <mpi.h>
 
-#define MAX_RAND 10
+#define N 1001
 #define DEFAULT_TAG 0
 
 void init_mpi(int* argc, char** argv[], int* rank, int* size){
@@ -14,21 +14,21 @@ void init_mpi(int* argc, char** argv[], int* rank, int* size){
 
 void my_MPI_send(int* number_buf, int number_amount, int receiver) {
   MPI_Send(number_buf, number_amount, MPI_INT, receiver, DEFAULT_TAG, MPI_COMM_WORLD);
-	printf("Sent %d numbers to %d\n", number_amount, receiver);
+//	printf("Sent %d numbers to %d\n", number_amount, receiver);
 }
 
 
 void my_MPI_receive(int* number_buf, int number_amount, int sender){
   MPI_Recv(number_buf, number_amount, MPI_INT, sender, DEFAULT_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  int i;
-  printf("From %d with love :*\n", sender); 
-        for (i = 0; i < number_amount; i++) {
-  		printf("%d\n", number_buf[i]);
-  	}
+  // int i;
+  // printf("From %d with love :*\n", sender); 
+  //      for (i = 0; i < number_amount; i++) {
+  //		printf("%d\n", number_buf[i]);
+  //	}
 
 }
 
-void MPI_send_back_and_forth(int rank, int* number_buf, int number_amount) {
+void MPI_one_ping_pong(int rank, int* number_buf, int number_amount) {
   if (rank == 0) {
   	my_MPI_send(number_buf, number_amount, 1);
         my_MPI_receive(number_buf, number_amount, 1);
@@ -38,27 +38,47 @@ void MPI_send_back_and_forth(int rank, int* number_buf, int number_amount) {
   }
 }
 
+int* init_buf(int n){
+  int* buf = (int*) malloc(sizeof(int) * n);
+  int i;
+  for (i=0; i < n; i++){
+     buf[i] = -1;
+  }
+  return buf;
+}
+
+float measure_bandwidth(int rank, int number_amount) {
+  
+  int i;
+  double t1, t2;
+  int* number_buf = init_buf(number_amount);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  t1 = MPI_Wtime();
+  for (i = 0; i < N; i++){
+     MPI_one_ping_pong(rank, number_buf, number_amount);    
+  } 
+  t2 = MPI_Wtime();
+
+  int buff_size = sizeof(int)*number_amount;
+  double time = (t2 - t1)/((float) N);
+  
+  double thrtp = (double) (8*buff_size)/(1000000.0*time);
+
+  free(number_buf);
+  
+  return thrtp;
+}
 
 int main (int argc, char * argv[])
 {
   int rank, size;
   init_mpi(&argc, &argv, &rank, &size);
  
-  int number_amount = 10;
-  int* number_buf = (int*) malloc(sizeof(int) * number_amount);
-  int i; 
-  for (i = 0; i < number_amount; i++) {
-    number_buf[i] = -1;
-  }
-  MPI_send_back_and_forth(rank, number_buf, number_amount);
+  int number_amount = 1000000;
+  double thrtp = measure_bandwidth(rank, number_amount);
+  if (rank == 0) printf("Throughtput: %f [Mbit/s]\n", thrtp);
 
-  // if (rank == 0) {
- //	my_MPI_send(number_buf, number_amount, 1);
-  //} else if (rank == 1) {
-  //	my_MPI_receive(number_buf, number_amount, 0);
-  // }
-
-  free(number_buf);
   MPI_Finalize();
   return 0;
 }
