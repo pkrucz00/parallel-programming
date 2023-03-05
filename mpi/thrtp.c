f5a7cca067143b4c2a5d80f5544eac9d7af0b9f1
pkrucz00 <pawel.kruczkiewicz@gmail.com>
2023-03-05 08:13:45 +0100

    Wrapping p2p sending in function

--- mpi/thrtp.c
+++ mpi/thrtp.c
@@ -14,36 +14,49 @@ void init_mpi(int* argc, char** argv[], int* rank, int* size){
 
 void my_MPI_send(int* number_buf, int number_amount, int receiver) {
   MPI_Send(number_buf, number_amount, MPI_INT, receiver, DEFAULT_TAG, MPI_COMM_WORLD);
+	printf("Sent %d numbers to %d\n", number_amount, receiver);
 }
 
 
 void my_MPI_receive(int* number_buf, int number_amount, int sender){
   MPI_Recv(number_buf, number_amount, MPI_INT, sender, DEFAULT_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  int i;
+  printf("From %d with love :*\n", sender); 
+        for (i = 0; i < number_amount; i++) {
+  		printf("%d\n", number_buf[i]);
+  	}
+
 }
+
+void MPI_send_back_and_forth(int rank, int* number_buf, int number_amount) {
+  if (rank == 0) {
+  	my_MPI_send(number_buf, number_amount, 1);
+        my_MPI_receive(number_buf, number_amount, 1);
+  } else if (rank == 1) {
+        my_MPI_receive(number_buf, number_amount, 0); 
+  	my_MPI_send(number_buf, number_amount, 0);
+  }
+}
+
 
 int main (int argc, char * argv[])
 {
   int rank, size;
   init_mpi(&argc, &argv, &rank, &size);
  
-  int number_amount = 1;
+  int number_amount = 10;
   int* number_buf = (int*) malloc(sizeof(int) * number_amount);
   int i; 
-
-  if (rank == 0) {
-        for (i = 0; i < number_amount; i++) {
-  	    number_buf[i] = -1;
-        }
-
-	my_MPI_send(number_buf, number_amount, 1);
-	printf("0 sent %d numbers to 1\n", number_amount);
-  } else if (rank == 1) {
-  	my_MPI_receive(number_buf, number_amount, 0);
-        for (i = 0; i < number_amount; i++) {
-  		printf("%d\n", number_buf[i]);
-  	}
-
+  for (i = 0; i < number_amount; i++) {
+    number_buf[i] = -1;
   }
+  MPI_send_back_and_forth(rank, number_buf, number_amount);
+
+  // if (rank == 0) {
+ //	my_MPI_send(number_buf, number_amount, 1);
+  //} else if (rank == 1) {
+  //	my_MPI_receive(number_buf, number_amount, 0);
+  // }
 
   free(number_buf);
   MPI_Finalize();
