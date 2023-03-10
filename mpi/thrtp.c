58a27143b0199d91b94ced14c6bd43057d51c15d
pkrucz00 <pawel.kruczkiewicz@gmail.com>
2023-03-10 07:34:44 +0100

    Better delays

--- mpi/thrtp.c
+++ mpi/thrtp.c
@@ -5,9 +5,9 @@
 
 #define N 1001
 #define DEFAULT_TAG 0
-#define TEST_CASES_NUM 6
+#define TEST_CASES_NUM 12
 #define CSV_NAME "results/normal_1_node.csv"
-#define BUFFERED 0
+#define BUFFERED 1
 
 void init_mpi(int* argc, char** argv[], int* rank, int* size){
    MPI_Init (argc, argv);  /* starts MPI */
@@ -17,14 +17,14 @@ void init_mpi(int* argc, char** argv[], int* rank, int* size){
 
 void my_MPI_send(int* number_buf, int number_amount, int receiver) {
   # ifdef BUFFERED
-        int buffer_attached_size = MPI_BSEND_OVERHEAD +  number_amount*sizeof(int);
-        char* buffer_attached = (char*) malloc(buffer_attached_size);
-        MPI_Buffer_attach(buffer_attached, buffer_attached_size);
+   int buffer_attached_size = MPI_BSEND_OVERHEAD +  number_amount*sizeof(int);
+   char* buffer_attached = (char*) malloc(buffer_attached_size);
+   MPI_Buffer_attach(buffer_attached, buffer_attached_size);
 
-        MPI_Bsend(number_buf, number_amount, MPI_INT, receiver, DEFAULT_TAG, MPI_COMM_WORLD);
+   MPI_Bsend(number_buf, number_amount, MPI_INT, receiver, DEFAULT_TAG, MPI_COMM_WORLD);
         
-        MPI_Buffer_detach(&buffer_attached, &buffer_attached_size);
-        free(buffer_attached); 
+   MPI_Buffer_detach(&buffer_attached, &buffer_attached_size);
+   free(buffer_attached); 
   # else
   MPI_Send(number_buf, number_amount, MPI_INT, receiver, DEFAULT_TAG, MPI_COMM_WORLD);
   # endif /* BUFFERED */
@@ -91,20 +91,20 @@ int main (int argc, char * argv[])
   int rank, size;
   init_mpi(&argc, &argv, &rank, &size);
  
-  const int number_amounts[TEST_CASES_NUM] = {100, 1000, 10000, 100000,  1000000, 10000000};
+  const int number_amounts[TEST_CASES_NUM] = {100, 250, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000};
   double* thrtps = (double*) malloc(sizeof(double)*TEST_CASES_NUM);
   int* buff_size = (int*) malloc(sizeof(int)*TEST_CASES_NUM);
   int i;  
-  for (i = 0; i < TEST_CASES_NUM; i++) {
-  	double snd_rcv_time_sec = measure_ping_pong_time(rank, number_amounts[i]);
+   for (i = 0; i < TEST_CASES_NUM; i++) {
+ 	double snd_rcv_time_sec = measure_ping_pong_time(rank, number_amounts[i]);
   	buff_size[i] = sizeof(int)*number_amounts[i];
         thrtps[i] = compute_thrtp(snd_rcv_time_sec, number_amounts[i]); 
   } 
-  double delay = 1000.0 * measure_ping_pong_time(rank, 1);  
+  // double delay = 1000.0 * measure_ping_pong_time(rank, 1);  
 
   if (rank == 0) {
   	export_to_csv(buff_size, thrtps);
-        printf("Delay: %f[ms]\n", delay);
+       //  printf("%f\n", delay);
   	
   }
   free(thrtps);
